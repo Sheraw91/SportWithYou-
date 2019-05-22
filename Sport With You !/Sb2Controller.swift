faec340c040de910da985d5260332022d51156af
Weder77 <dany.jc@yahoo.com>
2019-05-22 20:15:32 +0200

    SportWithYou 1.0

---
+++ Sport With You !/Sb2Controller.swift
@@ -0,0 +1,32 @@
+//
+//  Sb2Controller.swift
+//  Sport With You !
+//
+//  Created by Dany Jean-Charles on 22/05/2019.
+//  Copyright © 2019 Dany Jean-Charles. All rights reserved.
+//
+
+import UIKit
+
+class Sb2Controller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+      
+    }
+
+    @IBOutlet weak var number1View: UIImageView!
+    
+    @IBOutlet weak var text1Label: UILabel!
+    
+    @IBOutlet weak var number2View: UIImageView!
+    @IBOutlet weak var text2Label: UILabel!
+    
+    @IBOutlet weak var number3View: UIImageView!
+    @IBOutlet weak var text3Label: UILabel!
+    @IBOutlet weak var bar1View: UIView!
+    @IBOutlet weak var bar2View: UIView!
+}
+
