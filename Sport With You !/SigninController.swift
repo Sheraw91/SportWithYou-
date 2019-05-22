1f62e822e68f494c81e93ad324be0d570565aa52
Weder77 <dany.jc@yahoo.com>
2019-05-22 02:04:09 +0200

    beta 2.0

--- Sport With You !/SigninController.swift
+++ Sport With You !/SigninController.swift
@@ -26,6 +26,9 @@ class SigninController: UIViewController {
         
     }
     
+    //View for colo button
+    @IBOutlet weak var viewCenter: UIView!
+    @IBOutlet weak var viewCenter2: UIView!
     
     // Text Field
     @IBOutlet weak var firstnameTextfield: UITextField!
@@ -43,11 +46,41 @@ class SigninController: UIViewController {
     
     
     private func setupButtons() {
-        registerButton.layer.cornerRadius = 20
+        // Create a gradient layer for register
+        let gradient = CAGradientLayer()
+        // gradient colors in order which they will visually appear
+        gradient.colors = [UIColor.yellow.cgColor, UIColor.orange.cgColor]
+        // Gradient from left to right
+        gradient.startPoint = CGPoint(x: 0.0, y: 0.7)
+        gradient.endPoint = CGPoint(x: 1.0, y: 0.7)
+        // set the gradient layer to the same size as the view
+        gradient.frame = viewCenter.bounds
+        // add the gradient layer to the views layer for rendering
+        viewCenter.layer.insertSublayer(gradient, at: 0)
+        // Tha magic! Set the button as the views mask
+        viewCenter.mask = registerButton
+        //Set corner Radius and border Width of button
+        registerButton.layer.cornerRadius =  viewCenter.frame.size.height / 2
+        registerButton.layer.borderWidth = 5.0
         
-        loginButton.layer.cornerRadius = 20
-        loginButton.layer.borderWidth = 3
-        loginButton.layer.borderColor = UIColor.white.cgColor
+        // Create a gradient layer for login button
+        let gradient2 = CAGradientLayer()
+        // gradient colors in order which they will visually appear
+        gradient2.colors = [UIColor.orange.cgColor, UIColor.yellow.cgColor]
+        // Gradient from left to right
+        gradient2.startPoint = CGPoint(x: 0.0, y: 0.3)
+        gradient2.endPoint = CGPoint(x: 1.0, y: 0.3)
+        // set the gradient layer to the same size as the view
+        gradient2.frame = viewCenter2.bounds
+        // add the gradient layer to the views layer for rendering
+        viewCenter2.layer.insertSublayer(gradient2, at: 0)
+        // Tha magic! Set the button as the views mask
+        viewCenter2.mask = loginButton
+        //Set corner Radius and border Width of button
+        loginButton.layer.cornerRadius =  viewCenter2.frame.size.height / 2
+        loginButton.layer.borderWidth = 5.0
+        
+        
     }
     
     private func setupTextfieldsManaging() {
